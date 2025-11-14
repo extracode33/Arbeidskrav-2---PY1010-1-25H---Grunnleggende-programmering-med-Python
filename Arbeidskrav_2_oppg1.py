d943686326096e427a2609f39ea806edf1957675
extracode33 <vidar_due@hotmail.com>
2025-11-14 14:11:16 +0100

    Add files via upload

---
+++ Arbeidskrav_2_oppg1.py
@@ -0,0 +1,15 @@
+"""
+Arbeidskrav 2, oppgave 1
+Vidar Due (vidar_due@hotmail.com)
+Oppdatert 2025.11.14
+
+Programmet skal regne ut hvor gammel personen blir ila året 2024 ved å skrive inn årstall man er født
+
+"""
+
+
+alder = int(input("Hvilket år er du født?")) #fødselsår
+
+år = 2024
+
+print("Personen blir", år-alder, "år i løpet av 2024")
